dad5724c3ec0ff42f4d8118f12046beb5641afa8
jxc6125 <Esmeralda@owners-MacBook-Pro.local>
2019-06-07 12:47:25 -0400

    Initial commit

---
+++ src/Dealership/DealershipMain.java
@@ -0,0 +1,14 @@
+package Dealership;
+
+public class CarDealership {
+
+    CarDealership carDealership = new CarDealership();
+carDealership.setCarDealershipCode(“A1111”)
+carDealership.setCarDealershipLocation (“StateColleg,PA”)
+carDealership.setCarDealershipEmployees (“MikeBlack”)
+carDealership.setCarDealershipSponsers (“Honda”)
+carDealership.setCarDealershipProduct (“SUV”)
+carDealership.setCarDealershipStorage (“10”)
+carDealership.setCarDealershipRevenue (“100000”)
+carDealership.setCarDealershipCost (“50000”)
+}
---
+++ src/Dealership/Main.java
@@ -0,0 +1,29 @@
+package Dealership;
+
+public class carDealership {
+    public String carDealershipCode, carDealershipLocation, carDealershipEmployees,
+                    carDealershipSponsers, carDealershipProduct, carDealershipStorage,
+                    carDealershipRevenue, carDealershipCost;
+
+            public String getCarDealershipCode() {return carDealershipCode;}
+            public void setCarDealershipCode(String _CdsC){this.carDealershipCode = _CdsC;}
+            public String get carDealershipLocation () {return carDealershipLocation;}
+            public void setCarDealershipLocation (String _CdsL){this.carDealershipLocation =
+                    _CdsL;}
+            public String get carDealershipEmployees () {return carDealershipEmployees;}
+            public void setCarDealershipEmployees (String _CdsE){this.carDealershipEmployees =
+                    _CdsE;}
+            public String get carDealershipSponsers () {return carDealershipSponsers;}
+            public void setCarDealershipSponsers (String _CdsS){this.carDealershipSponsers =
+                    _CdsS;}
+            public String get carDealershipProduct () {return carDealershipProduct;}
+            public void setCarDealershipProduct (String _CdsP){this.carDealershipProduct =
+                    _CdsP;}
+            public String get carDealershipStorage () {return carDealershipStorage;}
+            public void setCarDealershipStorage (String _CdsSg){this.carDealershipStorage =
+                    _CdsSg;}
+            public String get carDealershipRevenue () {return carDealershipRevenue;}
+            public void setCarDealershipRevenue (String _CdsR){this.carDealershipRevenue =
+                    _CdsR;}
+            public String get carDealershipCost () {return carDealershipCost;}
+            public void setCarDealershipCost (String _CdsC){this.carDealershipCost= _CdsC;}    }
